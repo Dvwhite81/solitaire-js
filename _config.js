9eedbcd97eb1bc9eadc04ce57ffcfe8ca2e9a3b3
Dvwhite81 <Dvwhite1981@gmail.com>
2023-12-22 12:50:43 -0500

    Setup structure, images, css

--- _config.js
+++ _config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line unicorn/prefer-module
 module.exports = {
-  title: 'Checkers',
-  description: 'Checkers game built with JavaScript',
+  title: 'Solitaire',
+  description: 'Solitaire game built with JavaScript',
   keywords:
     'javascript, js, vanilla-javascript, vanilla-js, template, boilerplate, webpack, babel, sass, scss, css, github'
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import './styles/global.scss';
 // Allow dist on git for vercel
-
-const container = document.querySelector('#container');
-const h1 = document.createElement('h1');
-h1.textContent = 'Setup';
-container.append(h1);
