60a5a0f4c2a8f697550a210033f4ef5377890190
Dvwhite81 <Dvwhite1981@gmail.com>
2023-12-26 00:11:51 -0500

    experimenting with images

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,10 @@ module.exports = {
 			},
 			{
 				test: /\.(png|jpg|jpeg|gif|svg)$/i,
-				type: 'asset/resource'
+				type: 'asset/resource',
+				generator: {
+					filename: 'images/[name][ext][query]'
+				}
 			},
 			{
 				test: /\.(woff|woff2|eot|ttf|otf)$/i,
